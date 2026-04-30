b7cee35e8470bc1cf7b8607333f4de367345d16a
aledvch <alessandro.dvch@gmail.com>
2026-04-30 12:19:49 +0200

    Gallery: instant image switching, left/right click zones

--- app/components/GalleryBlock.tsx
+++ app/components/GalleryBlock.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import Image from 'next/image'
 import { useState } from 'react'
 import { urlFor } from '@/sanity/lib/image'
 import { RichText } from './RichText'
@@ -21,26 +20,38 @@ export function GalleryBlock({ images, caption }: GalleryBlockProps) {
   const [current, setCurrent] = useState(0)
   const isGallery = images.length > 1
 
+  const goNext = () => setCurrent((current + 1) % images.length)
+  const goPrev = () => setCurrent((current - 1 + images.length) % images.length)
+
   return (
     <div>
-      <div
-        className={isGallery ? 'cursor-pointer' : undefined}
-        onClick={
-          isGallery ? () => setCurrent((current + 1) % images.length) : undefined
-        }
-      >
+      <div style={{ position: 'relative' }}>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
         {images.map((img, i) => (
-          <div key={img._key ?? i} style={{ display: i === current ? 'block' : 'none' }}>
-            <Image
-              src={urlFor(img).width(1600).url()}
-              alt=""
-              width={img.dimensions?.width ?? 1600}
-              height={img.dimensions?.height ?? 900}
-              className="w-full h-auto"
-              priority={i === 0}
-            />
-          </div>
+          // eslint-disable-next-line @next/next/no-img-element
+          <img
+            key={img._key ?? i}
+            src={urlFor(img).width(1600).url()}
+            alt=""
+            style={{
+              display: i === current ? 'block' : 'none',
+              width: '100%',
+              height: 'auto',
+            }}
+          />
         ))}
+        {isGallery && (
+          <>
+            <div
+              style={{ position: 'absolute', top: 0, left: 0, width: '50%', height: '100%', cursor: 'w-resize' }}
+              onClick={goPrev}
+            />
+            <div
+              style={{ position: 'absolute', top: 0, right: 0, width: '50%', height: '100%', cursor: 'e-resize' }}
+              onClick={goNext}
+            />
+          </>
+        )}
       </div>
 
       {Array.isArray(caption) && caption.length > 0 && (
