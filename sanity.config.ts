3d63b548ff9fba36c07968cca2cdf75a487f96be
aledvch <alessandro.dvch@gmail.com>
2026-06-16 15:22:28 +0200

    Fix media plugin import

--- sanity.config.ts
+++ sanity.config.ts
@@ -7,7 +7,7 @@
 import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
-import {mediaPlugin} from 'sanity-plugin-media'
+import {media} from 'sanity-plugin-media'
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import {apiVersion, dataset, projectId} from './sanity/env'
@@ -23,6 +23,6 @@ export default defineConfig({
   plugins: [
     structureTool({structure}),
     visionTool({defaultApiVersion: apiVersion}),
-    mediaPlugin(),
+    media(),
   ],
 })
