6c0265af89c0a9cb60b4cbfa1a6c70fc2de80fe5
aledvch <alessandro.dvch@gmail.com>
2026-06-25 12:46:07 +0200

    About: remove cache, keep loading state

--- app/(main)/about/page.tsx
+++ app/(main)/about/page.tsx
@@ -20,7 +20,7 @@ interface About {
 }
 
 export default async function AboutPage() {
-  const about: About | null = await client.fetch(aboutQuery, {}, { next: { revalidate: 3600 } })
+  const about: About | null = await client.fetch(aboutQuery, {}, { next: { revalidate: 0 } })
 
   if (!about) {
     return (
